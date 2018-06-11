92cc5f6c9b618e5a0d647b07ba5590807a97bf6f
Jay Taylor <outtatime@gmail.com>
2018-06-10 18:36:53 -0700

    Fast bootstrap script.
    Useful for machines with only spinning rust, otherwise it could take
    days just to generate the tocrawl list.
    
    In my testing, this technique reduces the tocrawl time to ~5 minutes.
    
    Still 4x faster than my 2017 MacBook with SSD.

---
+++ misc/bootstrap-fast-ramdisk.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ $(id -u) -ne 0 ] ; then
+    echo 'error: must be run as root' 1>&2
+    exit 1
+fi
+
+set -x
+
+cd "$(dirname "$0")/.."
+
+mkdir -p /mnt/ramdisk
+
+while [ -n "$(mount | grep 'on \/mnt\/ramdisk' || :)" ] ; do
+    umount /mnt/ramdisk
+done
+
+mount -t tmpfs -o size=512m tmpfs /mnt/ramdisk
+
+if [ -L data ] ; then
+    unlink data
+fi
+
+if [ -e data ] ; then
+    dataBak="data.bak.$(date +%s)"
+    echo "INFO: Moving existing data dirrectory to ${dataBak}" 1>&2
+    mv data "${dataBak}"
+fi
+
+ln -s /mnt/ramdisk "$(pwd)/data"
+
+echo "INFO: Starting gcse-tocrawl" 1>&2
+gcse-tocrawl
+echo "INFO: Finished gcse-tocrawl" 1>&2
+
+echo -n 'INFO: Copying ramdisk data to stable storage ..' 1>&2
+newData="data.$(date +%s)"
+mkdir -p "${newData}"
+cp -a data/* "${newData}/"
+chown -R ppx:ppx data/
+echo 'OK'
+
+echo -n 'INFO: Cleaning up ramdisk ..' 1>&2
+unlink data
+umount /mnt/ramdisk
+mv "${newData}" data
+echo 'OK'
+
